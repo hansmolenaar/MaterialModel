c65d1a84752adc69fb20273b75a0c3786a3522cd
Hans Molenaar <molenaar.johannes@gmail.com>
2020-04-05 01:31:00 +0200

    Removed whitespace

--- RadiantApiSdk/IMaterialModel.cs
+++ RadiantApiSdk/IMaterialModel.cs
@@ -9,7 +9,6 @@ namespace MaterialModel.RadiantApiSdk
    public interface IMaterialModel
    {
       string DisplayName { get; }
-
       TopologicalSupport Support { get; }
       IEnumerable<IMaterialModelProperty> Elastic { get; }
       IEnumerable<IMaterialModelProperty> Inelastic { get; }
