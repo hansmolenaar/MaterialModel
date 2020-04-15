53612e44ec4d54bee4e0a331033a343e1d360e81
Hans Molenaar <molenaar.johannes@gmail.com>
2020-04-15 14:53:50 +0200

    Working version

---
+++ RadiantApiSdk/IMaterialModelPropertyGroup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MaterialModel.RadiantApiSdk
+{
+   public delegate bool BehaviorsConsistencyChecker(IReadOnlyList<string> choices, out string errorMessage);
+
+   public interface IMaterialModelPropertyGroup
+   {
+
+
+      IEnumerable<IMaterialModelProperty> Properties { get; }
+      BehaviorsConsistencyChecker ConistencyChecker { get; }
+
+
+   }
+}
